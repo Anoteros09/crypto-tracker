845f3f2e8b4fcecf8d67eefca207fd2de1f744de
Anoteros09 <insanity99coc@gmail.com>
2024-12-21 18:07:23 +0530

    Added Coin 1day history data + QOL features

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -1,31 +1,34 @@
+import { Paper } from "@mui/material";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav
-      className="navbar navbar-expand-lg 
+    <Paper elevation={3}>
+      <nav
+        className="navbar navbar-expand-lg 
                          navbar-dark bg-dark"
-    >
-      <div className="container">
-        <Link className="navbar-brand" href="/">
-          CryptoTracker
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto"></ul>
+      >
+        <div className="container">
+          <Link className="navbar-brand" href="/">
+            CryptoTracker
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto"></ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </Paper>
   );
 };
 
---
+++ src/app/utils/historicalDataFormatter.js
@@ -0,0 +1,24 @@
+function formatTimestamp(unixTimestamp) {
+  const date = new Date(unixTimestamp); // Convert UNIX timestamp (seconds) to milliseconds
+
+  const pad = (num) => String(num).padStart(2, "0"); // Function to pad single digits with a leading zero
+
+  const day = pad(date.getDate());
+  const month = pad(date.getMonth() + 1); // Months are zero-based in JavaScript
+  const year = date.getFullYear();
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+  const seconds = pad(date.getSeconds());
+
+  return new Date(year, month, day, hours, minutes, seconds);
+}
+
+export function historicalArrayFormatter(histData) {
+  const formattedData = [];
+  histData.map((data) => {
+    const price = data[1];
+    const date = formatTimestamp(data[0]);
+    formattedData.push({ price, date });
+  });
+  return formattedData;
+}
